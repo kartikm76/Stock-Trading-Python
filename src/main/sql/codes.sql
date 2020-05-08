ecc00d47e36eff4f5a8771a6fd08c386ad52f98e
kartikm76 <33011675+kartikm76@users.noreply.github.com>
2020-05-08 11:40:25 -0400

    removed sql folder

--- src/main/sql/codes.sql
+++
@@ -1,4 +0,0 @@
-insert into transaction_type_code (code, description) values ('B', 'Buy');
-insert into transaction_type_code (code, description) values ('S', 'Sell');
-insert into account_type_code (code, description) values ('TRD', 'Trading');
-
